77c486458c5cb18b3e380a39643333d3d21d5460
Deepak Gaur <your-email@gmail.com>
2026-06-20 17:53:23 +0530

    api response, error handling, add user and video model

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,27 @@
+const asyncHandler = (fn) => (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+};
+
+
+
+export{ asyncHandler }
+
+
+
+
+// const asyncHandle = () => {}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async () => {}
+
+    
+// const asyncHandle = (fn) => async (req, res, next) => {
+//     try {    Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+
+//         await fn(req, req, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json ({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
