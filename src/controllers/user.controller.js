c6348bac5c30c5bf44ebd75838324c8b15fe7a48
Deepak Gaur <your-email@gmail.com>
2026-06-27 17:40:39 +0530

    bug fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary} from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import { jwt } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 // const generateAccessAndRefreshTokens = async(userId) => {
 //     try{
@@ -215,7 +215,7 @@ const logoutUser = asyncHandler( async( req, res) => {
 
 })
 
-const refreshAccessToken = asyncHandler(async(res, res) => {
+const refreshAccessToken = asyncHandler(async(req, res) => {
     const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
 
     if ( incomingRefreshToken) {
