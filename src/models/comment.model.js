552b612acc61dfcfa7b8861e76a5c37c15b66c80
Deepak Gaur <deepakguar111@gmail.com>
2026-08-07 00:10:20 +0530

    feat: implement comment controller and routes for CRUD operations

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -1,5 +1,5 @@
 import mongoose, {Schema} from "mongoose";
-import mongooseAggregatePaginate from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const commentSchema = new Schema(
     {
@@ -9,17 +9,18 @@ const commentSchema = new Schema(
         },
         video: {
             type: Schema.Types.ObjectId,
-            ref: "Video",
+            ref: "Video"
         },
         owner: {
             type: Schema.Types.ObjectId,
-            ref: "User",
+            ref: "User"
         }
     },
     {
         timestamps: true
     }
 )
+
 
 commentSchema.plugin(mongooseAggregatePaginate)
 
